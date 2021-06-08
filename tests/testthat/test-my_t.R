f2c15243ef70f4ac692bcbfc4c79dc296ac019db
Alexander Thomson <athomson@Alexanders-MacBook-Pro.local>
2021-06-08 02:50:28 -0700

    Even more tests

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -32,3 +32,15 @@ test_that("Equal outputs, less", {
   expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$df,
                as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$parameter))
 })
+
+test_that("non-numeric mu input throws an error", {
+  expect_error(my_t.test(x = my_data$bill_depth_mm, alternative = "less", mu = "test"))
+})
+
+test_that("incorrect alternative input throws an error", {
+  expect_error(my_t.test(x = my_data$bill_depth_mm, alternative = 20, mu = 20))
+})
+
+test_that("incorrect alternative input throws an error", {
+  expect_error(my_t.test(x = data.frame(c("a", "b", "c")), alternative = 20, mu = 20))
+})
