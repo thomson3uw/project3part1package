0f48c09a3012256a7a42114b086ea27c338b178b
Alexander Thomson <athomson@Alexanders-MacBook-Pro.local>
2021-06-08 02:42:36 -0700

    Added a few tests

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -3,14 +3,32 @@ my_data <- stats::na.omit(project3part1package::my_penguins)
 test_that("Equal outputs, two.sided", {
   expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$test_stat,
                as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$statistic))
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$p_val,
+               as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$p.value))
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$alternative,
+               t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$alternative)
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$df,
+               as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "two.sided", mu = 20)$parameter))
 })
 
 test_that("Equal outputs, greater", {
   expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$test_stat,
                as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$statistic))
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$p_val,
+               as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$p.value))
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$alternative,
+               t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$alternative)
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$df,
+               as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "greater", mu = 20)$parameter))
 })
 
 test_that("Equal outputs, less", {
   expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$test_stat,
                as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$statistic))
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$p_val,
+               as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$p.value))
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$alternative,
+               t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$alternative)
+  expect_equal(my_t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$df,
+               as.numeric(t.test(x = my_data$bill_depth_mm, alternative = "less", mu = 20)$parameter))
 })
