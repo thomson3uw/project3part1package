7ffbf8b715d9b52c5ceae0bb97f8142437a53e9a
Alexander Thomson <athomson@Alexanders-MacBook-Pro.local>
2021-06-08 23:29:44 -0700

    Added a few tests for my_lm

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+my_data <- stats::na.omit(project3part1package::my_gapminder)
+
+test_that("Function output has the correct type", {
+  expect_type(my_lm(lifeExp ~ gdpPercap + continent, data = my_data), "double")
+})
+
+test_that("Function output has the correct dimension", {
+  expect_equal(length(my_lm(lifeExp ~ gdpPercap + continent, data = my_gapminder)[, 1]), 6)
+  expect_equal(length(my_lm(lifeExp ~ gdpPercap + continent, data = my_gapminder)[1, ]), 4)
+})
+
+test_that("Invalid formula throws an error", {
+  expect_error(my_lm(lifeExp ~ gdpPercap + continent + notavar, data = my_data))
+  expect_error(my_lm(formula = "string", data = my_data))
+})
+
+test_that("Invalid data throws an error", {
+  expect_error(my_lm(lifeExp ~ gdpPercap + continent, data = "some data"))
 })
